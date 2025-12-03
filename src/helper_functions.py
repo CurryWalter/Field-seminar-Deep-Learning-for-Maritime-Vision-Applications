8694c03c634ca755974ffecb6a6f8f7314b5228a
NyxLilith <github@lilyt.me>
2025-12-03 13:31:30 +0100

    match image to trajectory

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -7,3 +7,12 @@ def match_name_to_label(image_name):
             return row['label']
         continue
     return None
+
+def match_name_to_trajectory(image_name):
+    # input: image name output: label
+    df = create_lookup_table()
+    for _, row in df.iterrows():
+        if row['image_name'] == image_name:
+            return row['trajectory']
+        continue
+    return None
